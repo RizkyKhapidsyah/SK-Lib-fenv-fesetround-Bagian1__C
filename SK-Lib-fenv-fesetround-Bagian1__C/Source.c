6dbfb43663a20df13f7692fd721773201ecf450a
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-04-25 14:02:23 +0700

    Add project files.

---
+++ SK-Lib-fenv-fesetround-Bagian1__C/Source.c
@@ -0,0 +1,30 @@
+/* fesetround example */
+#include <stdio.h>      /* printf */
+#include <fenv.h>       /* fesetround, FE_* */
+#include <math.h>       /* rint */
+#include <conio.h>
+
+#pragma STDC FENV_ACCESS on
+
+/*	Source by CppBuzz
+*	Modified by Rizky Khapidsyah
+*/
+
+int main() {
+	printf("rounding -3.8:\n");
+
+	fesetround(FE_DOWNWARD);
+	printf("FE_DOWNWARD: %.1f\n", rint(-3.8));
+
+	fesetround(FE_TONEAREST);
+	printf("FE_TONEAREST: %.1f\n", rint(-3.8));
+
+	fesetround(FE_TOWARDZERO);
+	printf("FE_TOWARDZERO: %.1f\n", rint(-3.8));
+
+	fesetround(FE_UPWARD);
+	printf("FE_UPWARD: %.1f\n", rint(-3.8));
+	
+	_getch();
+	return 0;
+}
